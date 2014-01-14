8a4f96a2a52625e13cc56a83f41363b6281f4cb4
sunhyung <hikari1224@gmail.com>
2014-01-15 06:54:23 +0900

    First Commit

---
+++ src/JComparePython.py
@@ -0,0 +1,33 @@
+'''
+Created on Jan 14, 2014
+
+@author: hikari1224
+'''
+from JCompareCore import JCompareCore
+
+def doMain():
+	srcPath = "Sample Source File Path"
+	dstPath = "Sample Destination File Path"
+	core = JCompareCore()
+	
+	f = open(srcPath, "r")
+	while 1:
+		line = f.readline()
+		if not line: break
+		core.addSourceLine(line)
+	f.close()
+	
+	f = open(dstPath, "r")
+	while 1:
+		line = f.readline()
+		if not line: break
+		core.addDestinationLine(line)
+	f.close()
+	
+	core.doCompare();
+	for ce in core.compareResult:
+		print "Source line : %d, Destination line : %d, Line count of same contents : %d" % (ce.srcStartIndex, ce.dstStartIndex, ce.lineCountOfSameContext)		
+
+if __name__ == '__main__':
+	doMain()
+	pass
